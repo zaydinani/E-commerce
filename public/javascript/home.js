a05195561093a5dd3813ad0b60cbf43d081e3525
zaydinani <zaydinani@protonmail.com>
2023-03-03 22:08:17 +0100

    finished MVC setup

---
+++ public/javascript/home.js
@@ -0,0 +1,15 @@
+
+
+
+
+//carousel
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 4,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
