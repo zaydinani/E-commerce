6ea0d4dfbcf1c56603252bc127d536dbeb3204b1
zaydinani <zaydinani@protonmail.com>
2023-03-16 22:44:28 +0100

    added user registration and authentication

--- controllers/admin.js
+++ controllers/admin.js
@@ -2,40 +2,46 @@
 exports.getDashboard = (req, res, next) => {
     res.render('./admin/dashboard-main', {
         pageTitle: 'dashboard',
-        path: '/dash'
+        path: '/dash',
+        isLoggedIn: req.session.isLoggedIn
     });
 };
 
 exports.getAdminDashboard = (req, res, next) => {
     res.render('./admin/dashboard-admins', {
         pageTitle: 'dashboard-admins',
-        path: '/dash/admin'
+        path: '/dash/admin',
+        isLoggedIn: req.session.isLoggedIn
     })
 };
 
 exports.getCustomersDashboard = (req, res, next) => {
     res.render('./admin/dash-customers', {
         pageTitle: 'dashboard-customers',
-        path: '/dash/customers'
+        path: '/dash/customers',
+        isLoggedIn: req.session.isLoggedIn
     })
 };
 
 exports.getProductsDashboard = (req, res, next) => {
     res.render('./admin/dashboard-products', {
         pageTitle: 'dashboard-products',
-        path: '/dash/products'
+        path: '/dash/products',
+        isLoggedIn: req.session.isLoggedIn
     })
 };
 
 exports.getOrdersDashboard = (req, res, next) => {
     res.render('./admin/dashboard-orders', {
         pageTitle: 'dashboard-orders',
-        path: '/dash/orders'
+        path: '/dash/orders',
+        isLoggedIn: req.session.isLoggedIn
     })
 };
 exports.getSellersDashboard = (req, res, next) => {
     res.render('./admin/dashboard-sellers', {
         pageTitle: 'dashboard-sellers',
-        path: '/dash/seller'
+        path: '/dash/seller',
+        isLoggedIn: req.session.isLoggedIn
     })
 };
--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,11 @@ const express = require('express');
 
 const authController = require('../controllers/auth');
 const router = express.Router();
-// user authentication and registration routes
+//GET user authentication and registration routes
 router.get('/sign-up', authController.getSignUp);
 router.get('/sign-in', authController.getSignIn);
 
-
+//POST user authentication and registration routes
+router.post('/sign-up', authController.postSignUp);
+router.post('/sign-in', authController.postSignIn);
 module.exports = router;
