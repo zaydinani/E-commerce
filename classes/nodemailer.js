9dd41ef4fd81585c1c9f9ccdd65d2be8b6d8e88d
zaydinani <zaydinani@protonmail.com>
2023-06-19 20:45:48 +0100

    fixes

--- classes/nodemailer.js
+++ classes/nodemailer.js
@@ -1,33 +1,32 @@
-const nodemailer = require('nodemailer');
+const nodemailer = require("nodemailer");
 
 class NodeMailer {
-    constructor() {
-        this.transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: 'solideye99@gmail.com',
-                pass: 'oavahsxvbvvrqzfw'
-            }
-        })
-    }
-    sendMail(to, subject, htmlContent) {
-        let mailOptions = {
-            from: 'solideye99@gmail.com',
-            to: to,
-            subject: subject,
-            html: htmlContent
-        }
-        this.transporter.sendMail(mailOptions, function(error, info){
-            if (error) {
-                console.error(error)
-            } else {
-                console.log('email sent successfully')
-            }
-        })
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      service: `${process.env.NODEMAILER_SERVICE}`,
+      auth: {
+        user: `${process.env.NODEMAILER_EMAIL}`,
+        pass: `${process.env.NODEMAILER_PASSWORD}`,
+      },
+    });
+  }
+  sendMail(to, subject, htmlContent) {
+    let mailOptions = {
+      from: `${process.env.NODEMAILER_EMAIL}`,
+      to: to,
+      subject: subject,
+      html: htmlContent,
     };
+    this.transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.error(error);
+      } else {
+        console.log("email sent successfully");
+      }
+    });
+  }
 }
-module.exports = NodeMailer
-
+module.exports = NodeMailer;
 
 /*
 
